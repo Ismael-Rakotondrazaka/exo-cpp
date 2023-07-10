7c2903529a718f840a166692875d18eeb712149d
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-07-10 21:04:39 +0300

    feat(exo14.cpp): Make the exo14

---
+++ src/L2_TD_CPP_2023.docx/exo14/exo14.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+class Heure
+{
+private:
+    int heure;
+    int minute;
+
+public:
+    Heure(int, int);
+    ~Heure();
+    void ecritInfo();
+};
+
+Heure::Heure(int heure, int minute)
+{
+    this->heure = heure;
+    this->minute = minute;
+}
+
+Heure::~Heure()
+{
+    cout << "Appel du destructeur" << endl;
+}
+
+void Heure::ecritInfo()
+{
+    int futurHeure = this->heure;
+    int futureMinute = this->minute + 1;
+
+    if (futureMinute == 60)
+    {
+        futureMinute = 0;
+        futurHeure++;
+    }
+
+    if (futurHeure == 24)
+    {
+        futurHeure = 0;
+    }
+
+    cout << "Dans une minute, il sera " << futurHeure << " heures " << futureMinute << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    Heure *heure = new Heure(23, 59);
+    heure->ecritInfo();
+
+    return 0;
+}
